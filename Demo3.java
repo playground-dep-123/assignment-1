152db8167c57b0a254dd06025fe8a8a7315c5402
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-07-30 23:45:24 +0530

    Complete Task 3

---
+++ Demo3.java
@@ -0,0 +1,11 @@
+public class Demo3 {
+    
+    public static void main(String[] args) {
+        
+        int areaCode=123;
+        int secondPart=456;
+        int restPart=7890;
+
+        System.out.printf("(\033[34;1m%d\033[0m)-%d%d  \n",areaCode,secondPart,restPart);
+    }
+}
