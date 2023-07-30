9998f341a02ffbe204a2bdce4956531e0461bae7
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-07-31 01:10:17 +0530

    Complete Task 6-A

---
+++ Demo6_A.java
@@ -0,0 +1,9 @@
+public class Demo6_A {
+    public static void main(String[] args) {
+
+    
+        
+        System.out.printf("\033[41;1m[ %10s\033[0m\033[42;1m%s %10s ]\033[0m","5","0%","");
+    }
+    
+}
