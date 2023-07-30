671af4cd59449fe3806927dd7f38794e03853ae7
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-07-31 00:29:52 +0530

    Complete Task 5

---
+++ Demo5.java
@@ -0,0 +1,9 @@
+public class Demo5 {
+        public static void main(String[] args) {
+
+            
+
+            System.out.printf("\033[34;1m%d\033[0m-\033[31;1m%d\033[0m-\033[33;1m%d\033[0m-\033[37;1m%d\033[0m",part1,part2,part3,part4);
+            
+        }
+}
