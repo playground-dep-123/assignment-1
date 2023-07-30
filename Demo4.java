adbdd19e7b2d9d623787d56dcd1eccc156c8e2f0
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-07-31 00:22:55 +0530

    Complete Task 4

---
+++ Demo4.java
@@ -0,0 +1,17 @@
+public class Demo4 {
+    
+    public static void main(String[] args) {
+
+        int day=28;
+        int month=12;
+        int year=2023;
+        int hour=23;
+        int minute=59;
+        int second=59;
+
+    
+
+        System.out.printf("\033[34;1m%d\033[34;0m/\033[35;1m%d\033[34;0m/\033[36;1m%d\033[0m \033[30;1m%d\033[0m:\033[32;1m%d\033[0m:\033[31;1m%d \n",day,month,year,hour,minute,second);
+        
+    }
+}
